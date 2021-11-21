e6232de997d5eef26eafe5919f4c0a5c7608ba4f
JuaniOlguin <juani.olguin@gmail.com>
2021-11-21 10:38:55 -0300

    ''

--- src/app.js
+++ src/app.js
@@ -59,7 +59,7 @@ app.use(function(err, req, res, next) {
 });
 
 //rutas
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
